578384451e3c2ec8019183fbd51d8fceef2b6126
aeg <aeg@amazon.com>
2021-01-16 17:58:33 +1100

    Use english stopwords

--- src/similarity/cosine_similarity_comparer.py
+++ src/similarity/cosine_similarity_comparer.py
@@ -10,7 +10,7 @@ from similarity.base_similarity_comparer import BaseSimilarityComparer
 class CosineSimilarityComparer(BaseSimilarityComparer):
 
     def __init__(self, ngram):
-        self._count_vectoriser = CountVectorizer(stop_words=None, ngram_range=(ngram, ngram))
+        self._count_vectoriser = CountVectorizer(stop_words='english', ngram_range=(ngram, ngram))
 
     def __call__(self, source: List[str], target: List[str]) -> List[float]:
         try:
