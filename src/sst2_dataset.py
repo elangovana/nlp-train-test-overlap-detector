3b23726c13e635ebf915d3c76e01b70eecb492ac
aeg <aeg@amazon.com>
2021-01-20 23:26:33 +1100

    Add SST2 splitter

--- src/sst2_dataset.py
+++ src/sst2_dataset.py
@@ -3,8 +3,10 @@ import logging
 import sys
 
 import pandas as pd
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 
 from similarity.similarity_evaluator import SimilarityEvaluator
+from similarity.similarity_splitter import SimilaritySplitter
 
 TRAINID = 1
 TESTID = 2
@@ -41,7 +43,7 @@ class SST2Dataset:
                     missing_sen += 1
                     continue
                 phrase_id = dictionary_phrase_map[text]
-                id_data_map[split_id].append({"text": text, "label": phrase_sentiment[phrase_id]})
+                id_data_map[split_id].append({"text": text, "label": phrase_sentiment[phrase_id], "id": phrase_id})
 
             self._logger.warning("A {} out of {} were not found in dictionary".format(missing_sen, total_sen))
 
@@ -54,7 +56,18 @@ class SST2Dataset:
             for l in f.readlines()[1:]:
                 phrase_id, confidence = l.split(sep)[0], l.split(sep)[1].rstrip("\n")
                 phrase_id = int(phrase_id)
-                phrase_sentiment[phrase_id] = float(confidence)
+                phrase_sentiment[phrase_id] = "Negative" if float(confidence) < 0.5 else "Positive"
+
+        return phrase_sentiment
+
+    def _load_predictions(self, label_file):
+        phrase_sentiment = {}
+        sep = "|"
+        with open(label_file, "r") as f:
+            for l in f.readlines():
+                phrase_id, sentiment = l.split(sep)[0], l.split(sep)[1].rstrip("\n")
+                phrase_id = int(phrase_id)
+                phrase_sentiment[phrase_id] = sentiment
 
         return phrase_sentiment
 
@@ -87,6 +100,154 @@ class SST2Dataset:
         result_score, result_detail = SimilarityEvaluator().run(test, train, column="text")
         return result_score, result_detail
 
+    def run_similarity_threshold_splitter(self, predictionsfile, thresholds=None,
+                                          ngram=None):
+        train, test = self.load()
+
+        # Calculate scores based on similarity thresholds
+        thresholds = thresholds or [0, .00001, 25, 50, 75, 100]
+        ngram = ngram or [1, 2, 3]
+        result_split_summary = []
+
+        prediction_map = self._load_predictions(predictionsfile)
+        test["predictions"] = test["id"].apply(lambda x: prediction_map[x])
+        test["actual"] = test["label"]
+        for n in ngram:
+            self._logger.info("Splitting based on ngram {}".format(n))
+
+            for i in range(len(thresholds) - 1):
+                min_t = thresholds[i]
+                # For the last threshold send as None ..
+                max_t = thresholds[i + 1] if (i + 1) < (len(thresholds) - 1) else None
+                self._logger.info("Splitting on threshold {}-{}".format(min_t, max_t))
+
+                splitter = SimilaritySplitter(ngram=n, column="text")
+
+                # Split test based on similarity score
+                df = splitter.get(test, train, min_t, max_t)
+
+                accuracy = accuracy_score(df["actual"], df["predictions"])
+                f1 = f1_score(df["actual"], df["predictions"], pos_label="Positive", average="binary")
+                p = precision_score(df["actual"], df["predictions"], pos_label="Positive", average="binary")
+                r = recall_score(df["actual"], df["predictions"], pos_label="Positive", average="binary")
+
+                result_split_summary.append(
+                    {"ngram": n,
+                     "min": min_t,
+                     "max": max_t,
+                     "num": len(df),
+                     "percent": len(df) * 100 / len(test),
+                     "f-score": f1,
+                     "precision": p,
+                     "recall": r,
+                     "accuracy": accuracy
+                     }
+                )
+
+        # Full score
+        accuracy = accuracy_score(test["actual"], test["predictions"])
+        f1 = f1_score(test["actual"], test["predictions"], pos_label="Positive", average="binary")
+        p = precision_score(test["actual"], test["predictions"], pos_label="Positive", average="binary")
+        r = recall_score(test["actual"], test["predictions"], pos_label="Positive", average="binary")
+
+        result_split_summary.append(
+            {"ngram": -1,
+             "min": 0,
+             "max": 100,
+             "num": len(test),
+             "percent": len(test) * 100 / len(test),
+             "f-score": f1,
+             "precision": p,
+             "recall": r,
+             "accuracy": accuracy
+             }
+        )
+        return result_split_summary
+
+    def run_similarity_parts_splitter(self, predictionsfile, num_parts=4):
+        """
+Splits the results into n parts based sorted by similarity
+        :param comparison_type:
+        :param trainfile:
+        :param testfile:
+        :param outputdir:
+        :param test_gene_file:
+        :param test_alt_gene_file:
+        :param prediction_file:
+        :param num_parts:
+        :return:
+        """
+        train_df, test_df = self.load()
+
+        prediction_map = self._load_predictions(predictionsfile)
+        test_df["predictions"] = test_df["id"].apply(lambda x: prediction_map[x])
+        test_df["actual"] = test_df["label"]
+
+        result_split_summary = []
+        result_split_df = []
+
+        # Sort based on sim score
+        result_score, result_detail = self.run_similarity_comparer()
+        result_df = self._scores_to_df(result_score, result_detail)
+        ngram, ngram_i = "Unigram", 1
+        test_df[ngram] = result_df[ngram]
+
+        test_df = test_df.sort_values(by=ngram)
+
+        part_size = test_df.shape[0] // num_parts
+        for start in range(0, test_df.shape[0], part_size):
+            end = min(start + part_size, test_df.shape[0])
+            part_df = test_df.iloc[start: end]
+
+            accuracy = accuracy_score(part_df["actual"], part_df["predictions"])
+            f1 = f1_score(part_df["actual"], part_df["predictions"], pos_label="Positive", average="binary")
+            p = precision_score(part_df["actual"], part_df["predictions"], pos_label="Positive", average="binary")
+            r = recall_score(part_df["actual"], part_df["predictions"], pos_label="Positive", average="binary")
+
+            result_split_summary.append(
+                {"ngram": ngram_i,
+                 "min": part_df[ngram].min(),
+                 "max": part_df[ngram].max(),
+                 "num": len(part_df),
+                 "percent": len(part_df) * 100 / len(test_df),
+                 "f-score": f1,
+                 "precision": p,
+                 "recall": r,
+                 "accuracy": accuracy
+                 }
+            )
+            result_split_df.append(part_df)
+
+        # Full score
+        accuracy = accuracy_score(test_df["actual"], test_df["predictions"])
+        f1 = f1_score(test_df["actual"], test_df["predictions"], pos_label="Positive", average="binary")
+        p = precision_score(test_df["actual"], test_df["predictions"], pos_label="Positive", average="binary")
+        r = recall_score(test_df["actual"], test_df["predictions"], pos_label="Positive", average="binary")
+
+        result_split_summary.append(
+            {"ngram": -1,
+             "min": 0,
+             "max": 100,
+             "num": len(test_df),
+             "percent": len(test_df) * 100 / len(test_df),
+             "f-score": f1,
+             "precision": p,
+             "recall": r,
+             "accuracy": accuracy
+             }
+        )
+        return result_split_summary, result_split_df
+
+    def _scores_to_df(self, scores, details):
+        df = pd.DataFrame()
+        for k, v in scores.items():
+            df[k] = v
+
+        for k, v in details.items():
+            df[k + "_detail"] = v
+
+        return df
+
     @property
     def _logger(self):
         return logging.getLogger(__name__)
@@ -102,6 +263,8 @@ def run_main():
                         help="The dictionary file, dictionary.txt", required=True)
     parser.add_argument("--split",
                         help="The split file, e.g. datasetSplit.txt ", required=True)
+    parser.add_argument("--predictionsfile",
+                        help="The predictions file, e.g. predictions.txt ", required=True)
     parser.add_argument("--log-level", help="Log level", default="INFO", choices={"INFO", "WARN", "DEBUG", "ERROR"})
     args = parser.parse_args()
     print(args.__dict__)
@@ -112,6 +275,19 @@ def run_main():
     result_score, result_detail = SST2Dataset(args.sentencefile, args.sentiment, args.split,
                                               args.dictionary).run_similarity_comparer()
     SimilarityEvaluator().print_summary(result_score, result_detail)
+
+    if args.predictionsfile is not None:
+        print("--- Similarity parts splitter---")
+        result = SST2Dataset(args.sentencefile, args.sentiment, args.split,
+                             args.dictionary).run_similarity_parts_splitter(
+            args.predictionsfile)
+        print(result)
+
+        result, _ = SST2Dataset(args.sentencefile, args.sentiment, args.split,
+                                args.dictionary).run_similarity_threshold_splitter(
+            args.predictionsfile)
+        print("--- Similarity threshold splitter---")
+        print(result)
 
 
 if "__main__" == __name__:
